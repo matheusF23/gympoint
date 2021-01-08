5959018247d975957043d0a19fdd880dd21193b3
Matheus Figueiredo <matheus_figueiredo18@hotmail.com>
2021-01-08 15:25:26 -0300

    Created student update

--- src/app/controllers/StudentController.js
+++ src/app/controllers/StudentController.js
@@ -36,6 +36,53 @@ class StudentController {
       height,
     });
   }
+
+  async update(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+      email: Yup.string().email(),
+      birth: Yup.date(),
+      weight: Yup.number(),
+      height: Yup.number(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
+    const { id } = req.params;
+
+    const newEmail = req.body.email;
+
+    const student = await Student.findByPk(id);
+
+    if (!student) {
+      return res.status(400).json({ error: 'Student does not exists' });
+    }
+
+    if (newEmail && newEmail !== student.email) {
+      const studentExists = await Student.findOne({
+        where: { email: req.body.email },
+      });
+
+      if (studentExists) {
+        return res.status(400).json({ error: 'Student already exists' });
+      }
+    }
+
+    const { name, email, birth, weight, height } = await student.update(
+      req.body
+    );
+
+    return res.json({
+      id,
+      name,
+      email,
+      birth,
+      weight,
+      height,
+    });
+  }
 }
 
 export default new StudentController();
